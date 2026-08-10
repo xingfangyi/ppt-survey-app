44432b82b6909c5e8073de2a59e98691a90806dd
xingfangyi <1309058522@qq.com>
2026-08-10 12:28:50 +0800

    add template_config.py
    Define color constants and text box dimensions for slides.

---
+++ ppt_generator/template_config.py
@@ -0,0 +1,25 @@
+from pptx.util import Inches
+
+ABB_RED = "FF000F"
+ABB_LILAC = "6764F6"
+BLACK = "111111"
+GRAY = "666666"
+
+TEXT_BOXES = {
+    "slide2_left": (Inches(0.8), Inches(1.6), Inches(4.0), Inches(4.5)),
+    "slide2_right": (Inches(5.0), Inches(1.6), Inches(4.0), Inches(4.5)),
+
+    "slide3_left": (Inches(0.8), Inches(1.7), Inches(4.0), Inches(4.5)),
+    "slide3_right": (Inches(5.0), Inches(1.7), Inches(4.0), Inches(4.5)),
+
+    "slide4_main": (Inches(0.9), Inches(1.8), Inches(8.1), Inches(4.4)),
+
+    "slide5_left": (Inches(0.8), Inches(1.7), Inches(4.0), Inches(4.4)),
+    "slide5_right": (Inches(5.0), Inches(1.7), Inches(4.0), Inches(4.4)),
+
+    "slide6_left": (Inches(0.8), Inches(1.7), Inches(4.0), Inches(4.4)),
+    "slide6_right": (Inches(5.0), Inches(1.7), Inches(4.0), Inches(4.4)),
+
+    "slide7_left": (Inches(1.0), Inches(1.9), Inches(3.4), Inches(4.0)),
+    "slide7_right": (Inches(5.2), Inches(1.9), Inches(3.4), Inches(4.0)),
+}
